59d34c6a1946777533afdc8689cbd1491d3884b6
eip <36315290+eip618@users.noreply.github.com>
2022-08-25 12:24:44 +1000

    fix contact form (maybe?)

--- freecontactformsettings.php
+++ freecontactformsettings.php
@@ -2,7 +2,7 @@
 
 $email_to = "eip@eiphax.tech"; // your email address
 $email_subject = "Message from contact form"; // email subject line
-$thankyou = "thankyou.htm"; // thank you page
+$thankyou = "thankyou.php"; // thank you page
 
 // if you update the question on the form -
 // you need to update the questions answer below
