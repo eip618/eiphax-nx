dea05d02a6bfcdbf84f1b275e53f851b207a1342
eip <36315290+eip618@users.noreply.github.com>
2022-11-28 12:44:12 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 		<li><a href="./index">Home</a>
 		<li><a href="./nutshell">What We Know</a></li>
 		<li><a href="./donate">Donations</a></li>
-		<li><a href="./contact.htm">Contact</a></li>
+		<li><a href="./contact">Contact</a></li>
 		<li><a href="https://eiphax.tech/privacy" target="_blank">Privacy</a>
 		<li><a href="https://3ds.eiphax.tech">3DS</a></li>
 	</ul>
